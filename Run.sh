b379a0281edfff3ee2a2b4b18d550500e73b81b4
namoo-san <highgraphics.x@gmail.com>
2019-03-12 16:13:56 +0900

    Fixed divvy settings

--- Run.sh
+++ Run.sh
@@ -94,4 +94,3 @@ cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
 fc-cache -vf
 
 sh ./VSCode-Plugins.sh
-sudo cp ./com.mizage.direct.Divvy.plist ~/Library/Preferences/com.mizage.direct.Divvy.plist
