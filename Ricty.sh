952ee96fb4bb630ae608824a98d8eee7f5aa4ee0
namoo-san <highgraphics.x@gmail.com>
2020-02-28 09:27:00 +0900

    💬 Modified messages

--- Ricty.sh
+++ Ricty.sh
@@ -1,5 +1,5 @@
 # Ricty fonts install
-echo # Ricty fonts install...
+echo "Ricty fonts install..."
 brew tap sanemat/font
 brew install ricty
 cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
